3054c75d2ef2bec86b686ce50066e3a3e100cec8
huxibin12345 <huxibin_19601529@qq.com>
2016-04-28 17:57:25 +0800

    增加

--- WebContent/FPMovie/src/PlayPNGService.js
+++ WebContent/FPMovie/src/PlayPNGService.js
@@ -70,6 +70,18 @@ var PlayPNGService = {
 		/**
 		 * 加载数据
 		 */
+		loadPNGbykdmovie:function(movie_uuid,callback){
+			   var url="http://localhost/px-rest/rest/share/getFPMovie.js?kdmovie_uuid="+movie_uuid;//+"&t="+t;
+				 loadJS(url,callback);
+				 
+			//window.audio_mp3.play();
+			//
+
+	},
+		
+		/**
+		 * 加载数据
+		 */
 		loadPNG:function(movie_uuid,callback){
 			   var url="http://120.25.212.44/px-mobile/rest/share/getFPMovie.js?movie_uuid="+movie_uuid;//+"&t="+t;
 				 loadJS(url,callback);
