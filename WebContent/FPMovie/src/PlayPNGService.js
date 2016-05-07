d5f22a1d9e7714515e8acdf7d1264b6c49e0ae88
yjjewg11 <41049630@qq.com>
2016-05-07 21:08:02 +0800

    数据地址修改为正式系统。

--- WebContent/FPMovie/src/PlayPNGService.js
+++ WebContent/FPMovie/src/PlayPNGService.js
@@ -116,7 +116,7 @@ var PlayPNGService = {
 		 * 加载数据
 		 */
 		loadPNG:function(movie_uuid,callback){
-			   var url="http://120.25.212.44/px-mobile/rest/share/getFPMovie.js?movie_uuid="+movie_uuid;//+"&t="+t;
+			   var url="http://jz.wenjienet.com/px-mobile/rest/share/getFPMovie.js?movie_uuid="+movie_uuid;//+"&t="+t;
 				 loadJS(url,callback);
 				 
 			//window.audio_mp3.play();
